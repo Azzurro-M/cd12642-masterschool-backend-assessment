c21045a485052734768b72d704c638efa6d7fe88
Marianna <azzurromarianna@gmail.com>
2023-02-16 13:41:00 +0100

    part 7 done

--- controllers/favoritesController.js
+++ controllers/favoritesController.js
@@ -2,8 +2,54 @@
 const asyncHandler = require("express-async-handler");
 
 //add to favourtites
-const addIn = (req, res) => {
-  res.send("add in to fav");
-};
+const addIn = asyncHandler(async (req, res, next) => {
+  const { user, url, description, username, reason } = req.body;
 
-module.exports = { addIn };
+  const favoritePhoto = new FavoritePhoto({
+    user,
+    url,
+    description,
+    username,
+    reason,
+  });
+  await favoritePhoto.save();
+
+  res.status(201).json({ message: "Photo added to favorites" });
+});
+
+const getAllFavourites = asyncHandler(async (req, res, next) => {
+  const { user } = req.query;
+
+  const favoritePhotos = await FavoritePhoto.find({ user });
+  return res.json(favoritePhotos);
+});
+
+const deletePhoto = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const favoritePhoto = await FavoritePhoto.findById(id);
+  if (!favoritePhoto) {
+    res.status(404).json({ message: "Photo not found" });
+    return;
+  }
+
+  await favoritePhoto.remove();
+  res.json({ message: "Photo removed from favorites" });
+});
+
+const editPhoto = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const { description } = req.body;
+
+  const favoritePhoto = await FavoritePhoto.findById(id);
+  if (!favoritePhoto) {
+    res.status(404).json({ message: "Photo not found" });
+    return;
+  }
+
+  favoritePhoto.description = description;
+  await favoritePhoto.save();
+  res.json({ message: "Photo description updated" });
+});
+
+module.exports = { addIn, getAllFavourites, deletePhoto, editPhoto };
--- routes/favoritesRoutes.js
+++ routes/favoritesRoutes.js
@@ -1,8 +1,23 @@
 const authMiddleware = require("../middleware/authMiddleware.js");
 const express = require("express");
-const { addIn } = require("../controllers/favoritesController.js");
+const {
+  addIn,
+  getAllFavourites,
+  deletePhoto,
+  editPhoto,
+} = require("../controllers/favoritesController.js");
 const router = express.Router();
 
-router.get("/", authMiddleware.protect, addIn);
+//ADD PHOTO
+router.post("/", authMiddleware.protect, addIn);
+
+//GET ALL FAV PHOTOS
+router.get("/favouritePhotos", authMiddleware.protect, getAllFavourites);
+
+// DELETE PHOTO BY ID
+router.delete("/deletePhoto/:id", authMiddleware.protect, deletePhoto);
+
+//EDIT PHOTO DESCRIPTION
+router.patch("/editPhoto/:id", authMiddleware.protect, editPhoto);
 
 module.exports = router;
